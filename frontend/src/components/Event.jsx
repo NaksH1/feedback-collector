a128286c6fa1b9714e2bc3fddc1d04aa6b3f08a0
NaksH1 <nakshsinghhh1@gmail.com>
2024-08-21 13:22:28 +0530

    some css changes

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,5 +1,5 @@
 import AddIcon from '@mui/icons-material/Add';
-import { Alert, Card, Snackbar, Stack, TextField, Typography, Container } from "@mui/material";
+import { Alert, Card, Snackbar, Stack, TextField, Typography, Container, Box } from "@mui/material";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import AddEvent from './AddEvent';
@@ -54,8 +54,8 @@ function Event() {
 
         </Stack>
         <Stack direction="row" spacing={2} sx={{ marginTop: 2, display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-          {events.map((event) => {
-            return <Events event={event} key={event._id} />
+          {events.map((event, index) => {
+            return <Events event={event} key={event._id} index={index} />
           })}
         </Stack>
       </Container>
@@ -63,7 +63,7 @@ function Event() {
   )
 }
 
-export function Events({ event }) {
+export function Events({ event, index }) {
   const navigate = useNavigate();
   function handleEventClick() {
     navigate(`/events/${event._id}`);
@@ -74,22 +74,33 @@ export function Events({ event }) {
     return dayjs(date).format('Do MMMM YYYY');
   }
   const date = formatDate(event.date);
+  const backgroundColor = index % 3 === 0 ? '#dc9e40' : index % 2 === 0 ? '#d4782e' : '#b05912';
+  const clipPath = index % 2 === 0 ? 'polygon(0px 0px, 100% 0px, 100% 90%, 0% 98%)' : 'polygon(0px 0px, 100% 0px, 100% 98%, 0% 90%)';
   return (
     <div>
-      <Card sx={{ margin: 2, width: 300, minHeight: 200 }} onClick={handleEventClick}>
-        <img src={event.image} style={{ width: 300 }}></img>
-        <Typography textAlign={"center"} variant='h5'>
+      <Card sx={{
+        margin: 2, width: 300, minHeight: 200,
+        backgroundColor
+      }} onClick={handleEventClick}>
+        <img src={event.image} style={{
+          width: 300,
+          clipPath,
+          cursor: 'pointer',
+        }}></img>
+        <Typography textAlign={"center"} variant='h5' sx={{ color: '#fff' }}>
           {event.name}
         </Typography>
-        <Typography textAlign={"center"} variant='subtitle1'>
-          {date}
-        </Typography>
-        {event.programCoordinator ? <Typography textAlign={"center"} variant='subtitle1'>
-          {event.programCoordinator.name}
-        </Typography> : <></>}
-
+        <Box sx={{ backgroundColor: '#fff', marginTop: '2vh' }}>
+          <Typography textAlign={"center"} variant='subtitle1'>
+            {date}
+          </Typography>
+          {event.programCoordinator ? <Typography textAlign={"center"} variant='subtitle1'>
+            {event.programCoordinator.name}
+          </Typography>
+            : <></>}
+        </Box>
       </Card>
-    </div>
+    </div >
   )
 
 }
